c16e2852befa55936323cfd944bac66acba6c4fb
storezhang <storezhang@gmail.com>
2024-08-27 16:24:27 +0800

    feat(项目): 增加toml支持

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/goexl/config
 
 go 1.23
 
-require gopkg.in/yaml.v3 v3.0.1
+require (
+	github.com/pelletier/go-toml/v2 v2.2.3
+	gopkg.in/yaml.v3 v3.0.1
+)
--- slice.go
+++ slice.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"encoding/json"
+	"encoding/xml"
 
+	"github.com/pelletier/go-toml/v2"
 	"gopkg.in/yaml.v3"
 )
 
@@ -24,6 +26,28 @@ func (s *Slice[T]) UnmarshalYAML(node *yaml.Node) (err error) {
 	t := new(T)
 	if ue := node.Decode(t); nil != ue {
 		err = node.Decode(s)
+	} else {
+		*s = []*T{t}
+	}
+
+	return
+}
+
+func (s *Slice[T]) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) (err error) {
+	t := new(T)
+	if ue := decoder.DecodeElement(t, &start); nil != ue {
+		err = decoder.DecodeElement(s, &start)
+	} else {
+		*s = []*T{t}
+	}
+
+	return
+}
+
+func (s *Slice[T]) UnmarshalText(bytes []byte) (err error) {
+	t := new(T)
+	if ue := toml.Unmarshal(bytes, t); nil != ue {
+		err = toml.Unmarshal(bytes, s)
 	} else {
 		*s = []*T{t}
 	}
